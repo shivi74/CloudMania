a3e0780d49695f49ed864c6f019258c9b09ab696
Love Sharma <contact@lovesharma.com>
2014-03-30 12:44:38 +0530

    new approach and solved indentation errors

---
+++ database.py
@@ -0,0 +1,19 @@
+from google.appengine.ext import db
+
+Query = db.Query
+
+class User(db.Model):
+  email = db.StringProperty(required = True)
+  password = db.StringProperty(required = True)
+  created = db.DateTimeProperty(auto_now_add=True)
+  updated = db.DateTimeProperty(auto_now=True)
+
+class Verify(db.Model):
+  email = db.StringProperty(required = True)
+  uuid = db.StringProperty(required = True)
+  is_verify = db.BooleanProperty()
+
+class Forgot(db.Model):
+  email = db.StringProperty(required = True)
+  is_viewed = db.BooleanProperty()
+  password = db.TextProperty()
--- main.py
+++ main.py
@@ -9,30 +9,14 @@ import base64
 import urllib
 import datetime
 import smtplib
+import database
 
 from google.appengine.api import users
-from google.appengine.ext import db
 
 JINJA_ENVIRONMENT = jinja2.Environment(
     loader=jinja2.FileSystemLoader(os.path.dirname(__file__)),
     extensions=['jinja2.ext.autoescape'],
     autoescape=True)
-
-class User(db.Model):
-  email = db.StringProperty(required = True)
-  password = db.StringProperty(required = True)
-  created = db.DateTimeProperty(auto_now_add=True)
-  updated = db.DateTimeProperty(auto_now=True)
-  
-class Verify(db.Model):
-  email = db.StringProperty(required = True)
-  uuid = db.StringProperty(required = True)
-  is_verify = db.BooleanProperty()
-
-class Forgot(db.Model):
-  email = db.StringProperty(required = True)
-  is_viewed = db.BooleanProperty()
-  password = db.TextProperty()
   
 EMAIL_RE = re.compile(r"^[\S]+@[\S]+\.[\S]+$")
 def valid_email(email):
@@ -40,7 +24,8 @@ def valid_email(email):
 
 class MainPage(webapp2.RequestHandler):
   def get(self):
-    self.redirect('/')
+    template = JINJA_ENVIRONMENT.get_template('index.html')
+    self.response.out.write(template.render({}))
 
 class RegisterHandler(webapp2.RequestHandler):
 
@@ -53,18 +38,18 @@ class RegisterHandler(webapp2.RequestHandler):
     logging.info(valid_email(user_email))
     user_password = self.request.get('password')
     user_cpassword = self.request.get('confirmpassword')
-    q = db.Query(User)
+    q = database.Query(database.User)
     q.filter("email =", user_email)
     total = q.count()
     logging.info(total)
     if (user_email and valid_email(user_email)) and (user_password == user_cpassword) and total == 0:
-	User(email=user_email, password=base64.b64encode('user_password')).put();
-    	self.redirect('/verify')
-	return
+      database.User(email=user_email, password=base64.b64encode('user_password')).put()
+      self.redirect('/verify')
+      return
     else:
       errors = []
       if(total !=0):
-	errors.append("Email address already exists!")
+        errors.append("Email address already exists!")
       elif(user_email or valid_email(user_email)):
         errors.append("Email Address is not valid!")
       if(user_password != user_cpassword):
@@ -77,10 +62,7 @@ class RegisterHandler(webapp2.RequestHandler):
 
 class VerifyHandler(webapp2.RequestHandler):
 
-  def get(self, uuid):
-    self.redirect('/')
-
-  def post(self):
+  def get(self):
     logging.info(self.request)
     user_email = self.request.get('email')
     user_uuid = uuid.uuid5(uuid.NAMESPACE_DNS, 'user_email')
@@ -92,18 +74,18 @@ class VerifyHandler(webapp2.RequestHandler):
     msg = "\nHello! Click on the following link to verify: \n "
     link = "cloudmania.in/verify/?uuid=" + user_uuid
     server.sendmail("shivani.9487@gmail.com", "user_email", msg + link)
-    Verify(email=user_email, uuid=user_uuid, is_verify="false").put();
+    database.Verify(email=user_email, uuid=user_uuid, is_verify="false").put();
     user_uuidg = self.request.get('user_uuid')
-    if (user_uuid and user_uuidg)
-	Verify(email=user_email, uuid=user_uuid, is_verify="true").put();
+    if (user_uuid and user_uuidg):
+      Verify(email=user_email, uuid=user_uuid, is_verify="true").put();
     else:
       errors = []
       if(not Verify.is_verify):
-	errors.append("User not Verified!")
+        errors.append("User not Verified!")
     template_values = {"email":"","uuid":"","is_verify":""}
     template = JINJA_ENVIRONMENT.get_template('index.html')
     self.response.write(template.render(template_values))
-    
+
 class LoginHandler(webapp2.RequestHandler):
   def post(self):
     logging.info(self.request)
@@ -111,24 +93,23 @@ class LoginHandler(webapp2.RequestHandler):
     is_valid = valid_email(user_email)
     errors = []
     if (is_valid):
-    	user_password = self.request.get('password', '')
-	q = db.Query(User)
-	q.filter("email =", user_email)
-	record = q.fetch(1)
-	logging.info(record[0])
-	logging.info(dir(record[0]))
-	if(user_password == record[0].password):
-	  template_values = {'login': True, 'user': record[0].email}
+      user_password = self.request.get('password', '')
+      q = database.Query(User)
+      q.filter("email =", user_email)
+      record = q.fetch(1)
+      logging.info(record[0])
+      logging.info(dir(record[0]))
+      if(user_password == record[0].password):
+        template_values = {'login': True, 'user': record[0].email}
     if( not is_valid):
-	errors.append('Wrong Username / Password!')
-	template_values = {'errors': '<br/>'.join(errors), 'login': True}
+      errors.append('Wrong Username / Password!')
+    template_values = {'errors': '<br/>'.join(errors), 'login': True}
     template = JINJA_ENVIRONMENT.get_template('index.html')
     self.response.out.write(template.render(template_values))
 
 app = webapp2.WSGIApplication([
     ('/', MainPage),
-    ('/next', NextPage),
-    (r'/verify/(.*)$', VerifyHandler),
+    ('/verify', VerifyHandler),
     ('/register', RegisterHandler),
     ('/login', LoginHandler),
 ], debug=True)
