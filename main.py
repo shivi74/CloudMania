86fb0f58dc7937dcc015a9a156a57cde61547acb
Shivani <shivani.9487@gmail.com>
2014-03-30 14:01:14 +0530

    .

--- main.py
+++ main.py
@@ -58,13 +58,13 @@ class RegisterHandler(webapp2.RequestHandler):
     total = q.count()
     logging.info(total)
     if (user_email and valid_email(user_email)) and (user_password == user_cpassword) and total == 0:
-	User(email=user_email, password=base64.b64encode('user_password')).put();
-    	self.redirect('/verify')
-	return
+      User(email=user_email, password=base64.b64encode('user_password')).put();
+      self.redirect('/verify')
+      return
     else:
       errors = []
       if(total !=0):
-	errors.append("Email address already exists!")
+        errors.append("Email address already exists!")
       elif(user_email or valid_email(user_email)):
         errors.append("Email Address is not valid!")
       if(user_password != user_cpassword):
@@ -94,12 +94,12 @@ class VerifyHandler(webapp2.RequestHandler):
     server.sendmail("shivani.9487@gmail.com", "user_email", msg + link)
     Verify(email=user_email, uuid=user_uuid, is_verify="false").put();
     user_uuidg = self.request.get('user_uuid')
-    if (user_uuid and user_uuidg)
-	Verify(email=user_email, uuid=user_uuid, is_verify="true").put();
+    if (user_uuid and user_uuidg):
+      Verify(email=user_email, uuid=user_uuid, is_verify="true").put();
     else:
       errors = []
       if(not Verify.is_verify):
-	errors.append("User not Verified!")
+        errors.append("User not Verified!")
     template_values = {"email":"","uuid":"","is_verify":""}
     template = JINJA_ENVIRONMENT.get_template('index.html')
     self.response.write(template.render(template_values))
@@ -111,17 +111,17 @@ class LoginHandler(webapp2.RequestHandler):
     is_valid = valid_email(user_email)
     errors = []
     if (is_valid):
-    	user_password = self.request.get('password', '')
-	q = db.Query(User)
-	q.filter("email =", user_email)
-	record = q.fetch(1)
-	logging.info(record[0])
-	logging.info(dir(record[0]))
-	if(user_password == record[0].password):
-	  template_values = {'login': True, 'user': record[0].email}
+      user_password = self.request.get('password', '')
+      q = db.Query(User)
+      q.filter("email =", user_email)
+      record = q.fetch(1)
+      logging.info(record[0])
+      logging.info(dir(record[0]))
+      if(user_password == record[0].password):
+        template_values = {'login': True, 'user': record[0].email}
     if( not is_valid):
-	errors.append('Wrong Username / Password!')
-	template_values = {'errors': '<br/>'.join(errors), 'login': True}
+      errors.append('Wrong Username / Password!')
+      template_values = {'errors': '<br/>'.join(errors), 'login': True}
     template = JINJA_ENVIRONMENT.get_template('index.html')
     self.response.out.write(template.render(template_values))
 
